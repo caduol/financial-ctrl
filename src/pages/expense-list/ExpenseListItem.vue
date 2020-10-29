e252bfb283f0de1fbbf385de3e440b8895767131
Cadu <eduardo.w3c@gmail.com>
2020-10-29 20:26:04 -0300

    fix

--- src/pages/expense-list/ExpenseListItem.vue
+++ src/pages/expense-list/ExpenseListItem.vue
@@ -2,7 +2,8 @@
   <div class="expense-list-item row">
     <div class="col-10">
       <small v-date-format="data.createdAt" />
-      <div>{{ data.description }}</div>
+      <span>{{ data.description }}</span>
+      <button class="btn btn-sm btn-outline-primary">Ver comprovante</button>
     </div>
 
     <div class="col-2">
@@ -23,5 +24,9 @@ export default {
 .expense-list-item {
   padding: 10px 0;
   border-bottom: 1px solid var(--dark-medium);
+  .description {
+    display: flex;
+    justify-content: space-between;
+  }
 }
 </style>
